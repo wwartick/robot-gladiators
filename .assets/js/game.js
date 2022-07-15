790824f1e1b39a4a1f0d52a2aa215e9ebd4419b9
wwartick <willwartick@gmail.com>
2022-07-14 17:54:15 -0700

    start and end game function

--- .assets/js/game.js
+++ .assets/js/game.js
@@ -123,7 +123,50 @@ var fight = function(enemyName){
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
     } //end of while loop
-}; //end of fight function
+} //end of fight function
+
+//function to start a new game
+
+var endGame = function() {
+
+    if (playerHealth > 0) {
+  
+      window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  
+    }
+  
+    else{
+  
+      window.alert("You've lost your robot in battle.");
+  
+    }
+  
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+  
+    if (playAgainConfirm) {
+  
+      startGame();
+  
+    }
+  
+    else {
+  
+      window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  
+  }
+  
+  }
+  
+
+var startGame = function() {
+
+    //reset player stats
+
+    playerHealth = 100;
+
+    playerAttack = 10;
+
+    playerMoney = 10;
 
 for(var i = 0; i < enemyNames.length; i++) {
 
@@ -151,6 +194,7 @@ if (playerHealth>0) {
 
 }
 
+
 else {
 
     window.alert("You have lost your robot in battle! Game Over!");
@@ -159,4 +203,14 @@ else {
 
 }
 
-}
+   
+
+    }  //end of start game function
+    
+    endGame();
+} 
+
+startGame();
+
+
+
